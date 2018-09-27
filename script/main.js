fbf859a7002919402b6b1c6db7ca248a308aef7c
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-09-27 22:02:41 +0100

    Refactor code according to AirBnb syntax

--- script/main.js
+++ script/main.js
@@ -1,18 +1,21 @@
-function calc() {
+const compute = function processUserInputAndDisplayResult(){
   // retreive and clean user input
   userInput = document.getElementById('input').value.replace(/\s+/g, '');
   // prevent code injection
   acceptedInput = /^(?![\+\-\*\/\%\^])(?!.*[\+\-\*\/\%\^]$)(?!.*?[\+\-\*\/\%\^][\+\-\*\/\%\^])(sqrt\(\d+\)|\d|\+|\-|\*|\/|\%|\^|\(|\))+?$/gi
   if (!acceptedInput.test(userInput)) {
     document.getElementById('result').textContent = "Error: uncorrect syntax.";
-    return false;
+    return 1;
   }
+  // display result
+  document.getElementById('result').textContent = calc(userInput);
+  return 0
+}
+
+const calc = function evaluateUserInput() {
   // pepare code for evaluation
   userInput = userInput.replace(/sqrt/ig, 'Math.sqrt');
   userInput = userInput.replace(/\^/ig, '**');
   // compute and display result
-  result = eval(userInput);
-  document.getElementById('result').textContent = result;
-  // prevent page to refresh (and to reset #result.textContent)
-  return false;
+  return eval(userInput);
 }
