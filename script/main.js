e5cee4f1368d28368846f4b4a35bb3f89b9d4355
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-09-27 22:03:11 +0100

    Accept operations starting with negative number

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ const compute = function processUserInputAndDisplayResult(){
   // retreive and clean user input
   userInput = document.getElementById('input').value.replace(/\s+/g, '');
   // prevent code injection
-  acceptedInput = /^(?![\+\-\*\/\%\^])(?!.*[\+\-\*\/\%\^]$)(?!.*?[\+\-\*\/\%\^][\+\-\*\/\%\^])(sqrt\(\d+\)|\d|\+|\-|\*|\/|\%|\^|\(|\))+?$/gi
+  acceptedInput = /^(?![\+\*\/\%\^])(?!.*[\+\-\*\/\%\^]$)(?!.*?[\+\-\*\/\%\^][\+\-\*\/\%\^])(sqrt\(\d+\)|\d|\+|\-|\*|\/|\%|\^|\(|\))+?$/gi
   if (!acceptedInput.test(userInput)) {
     document.getElementById('result').textContent = "Error: uncorrect syntax.";
     return 1;
