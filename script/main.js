acaf145a5bd75ba914a1fdd15c28638d5faee204
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-09-27 21:47:18 +0100

    Accept sqrt and ^ as operators

--- script/main.js
+++ script/main.js
@@ -1,28 +1,18 @@
 function calc() {
   // retreive and clean user input
   userInput = document.getElementById('input').value.replace(/\s+/g, '');
-  console.log('ui: ' + userInput);
   // prevent code injection
-  acceptedInput = /^(?![\+\-\*\/\%\^])(?!.*[\+\-\*\/\%\^]$)(?!.*?[\+\-\*\/\%\^][\+\-\*\/\%\^])(sqrt\(\d\)|\d|\+|\-|\*|\/|\%|\^|\(|\))+?$/gi
+  acceptedInput = /^(?![\+\-\*\/\%\^])(?!.*[\+\-\*\/\%\^]$)(?!.*?[\+\-\*\/\%\^][\+\-\*\/\%\^])(sqrt\(\d+\)|\d|\+|\-|\*|\/|\%|\^|\(|\))+?$/gi
   if (!acceptedInput.test(userInput)) {
-    document.getElementById('result').textContent = "ERROR";
+    document.getElementById('result').textContent = "Error: uncorrect syntax.";
     return false;
   }
   // pepare code for evaluation
   userInput = userInput.replace(/sqrt/ig, 'Math.sqrt');
+  userInput = userInput.replace(/\^/ig, '**');
+  // compute and display result
   result = eval(userInput);
-  console.log(result);
   document.getElementById('result').textContent = result;
+  // prevent page to refresh (and to reset #result.textContent)
   return false;
-}
-
-function parse_input(input) {
-  // solve sqrt case first
-  const sqrtArg = /^sqrt\((\d+)\)$/i;
-  if (sqrtArg.exec(input)) {
-    // not DRY proof!
-    // regex ensure sqrt argument is positive
-    return Math.sqrt(sqrtArg.exec(input)[1]);
-  }
-  return "Error";
 }
